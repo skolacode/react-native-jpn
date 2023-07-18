4b45c20202f21bb298de930e45f17fa2e38325cc
Mohamed Burhan <bern2727@gmail.com>
2023-07-18 10:11:01 +0800

    1. Beautify the styles in avatar section and profile section 2. use my own avatar image

--- App.js
+++ App.js
@@ -8,25 +8,29 @@ export default function App() {
       {/* Avatar Container */}
       <View style={styles.avatarContainer}> 
         <Image
-          style={{ width: 50, height: 50 }}
+          style={{ width: 100, height: 100 }}
           source={{
-            uri: 'https://reactnative.dev/img/tiny_logo.png',
+            uri: 'https://burhan3759.com/assets/my_logo.png',
           }}
         />
 
-        <Text>Burhan</Text>
+        <Text style={styles.nameText}>Burhan</Text>
       </View>
 
       {/* Pofile details container */}
       <View style={styles.profileContainer}> 
         
         <Text style={styles.labelText}>Email: 
-          <Text style={styles.valueText}>iam@burhan3759.com</Text>
+          <Text style={styles.valueText}> iam@burhan3759.com</Text>
         </Text>
         
-        <Text>Gender: <Text>Male</Text></Text>
+        <Text style={styles.labelText}>Gender: 
+          <Text style={styles.valueText}> Male</Text>
+        </Text>
         
-        <Text>Hobby: <Text>Coding..</Text></Text>
+        <Text style={styles.labelText}>Hobby: 
+          <Text style={styles.valueText}> Coding..</Text>
+        </Text>
       </View>
 
       <StatusBar style="auto" />
@@ -41,21 +45,32 @@ const styles = StyleSheet.create({
     backgroundColor: 'white',
   },
   avatarContainer: {
+    flex: 0.5,
     justifyContent: "center",
     alignItems: "center",
-    backgroundColor: 'lightyellow',
-    flex: 0.5,
+    margin: 20,
+    backgroundColor: '#faf7f8',
+    borderColor: '#e2e2e2',
+    borderWidth: 1,
+    borderRadius: 10,
   },
   profileContainer: {
     flex: 1,
-    padding: 20
+    padding: 20,
+  },
+  nameText: {
+    fontSize: 24,
+    color: '#242323',
+    marginTop: 10,
+    fontWeight: '600'
   },
   labelText: {
-    fontSize: 12,
-    color: '#4d4d4d'
+    fontSize: 16,
+    color: '#4d4d4d',
+    marginBottom: 10
   },
   valueText: {
-    fontSize: 14,
-    color: 'blue'
+    fontSize: 17,
+    color: '#171717',
   }
 });
