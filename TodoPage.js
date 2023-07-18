769885bf0f6bce0b6e6bf02fa4f170d08757b864
Mohamed Burhan <bern2727@gmail.com>
2023-07-18 15:10:13 +0800

    1. Added a text input field 2. add a button 3. add a style

--- TodoPage.js
+++ TodoPage.js
@@ -1,10 +1,50 @@
-import { View, Text } from 'react-native'
+import { View, Text, TextInput, StyleSheet, TouchableOpacity } from 'react-native'
 
 
 export default () => {
+
+  const onTextChange = () => { }
+
+  const addTodo = () => {
+    console.log('Todo is clicked')
+  }
+
   return (
     <View style={{ marginTop: 40 }}>
       <Text>This is Todo Page</Text>
+
+      <TextInput
+        style={styles.input}
+        onChangeText={onTextChange}
+        placeholder="useless placeholder"
+      />
+
+      <TouchableOpacity style={styles.button} onPress={addTodo}>
+        <Text style={styles.addText}>Add</Text>
+      </TouchableOpacity>
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+  input: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
+    borderRadius: 5
+  },
+  button: {
+    backgroundColor: '#d6d6d6',
+    margin: 10,
+    marginBottom: 20,
+    padding: 15,
+    borderRadius: 10,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  addText: {
+    fontWeight: '600',
+    color: '#3e3f40'
+  }
+});
